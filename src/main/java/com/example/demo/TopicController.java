e805de3fca306836437ed5a80a6fe677ae68ba2c
lumsdenitservices <59616032+lumsdenitservices@users.noreply.github.com>
2020-01-27 11:22:18 +0000

    Update TopicController.java
    Amend POST method.

--- src/main/java/com/example/demo/TopicController.java
+++ src/main/java/com/example/demo/TopicController.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,17 +18,18 @@ public class TopicController {
 	@Autowired
 	private TopicService topicService;
 	
-	@RequestMapping("/topics")
+	@GetMapping("/topics")
 	public List<Topic> getAllTopics() {
 		return topicService.getAllTopics();
 	}
 	
-	@RequestMapping("/topics/{id}")
+	@GetMapping("/topics/{id}")
 	public Topic getTopic(@PathVariable String id) {
 		return topicService.getTopic(id);
 	}
 	
-	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	//@PostMapping(method=RequestMethod.POST, value="/topics")
+	@PostMapping("/topics")
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
 	}
